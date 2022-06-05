7c131233cb0e0aaf3371b162ea5c464a82815d16
sssssqew <sunrise.in.my.dream@gmail.com>
2022-06-05 17:40:37 +0900

    simple code editor that is possible to download code

---
+++ app.js
@@ -0,0 +1,34 @@
+const htmlEditor = CodeMirror(document.querySelector('.editor .code .html-code'), {
+  lineNumbers: true,
+  tabSize: 2,
+  mode: 'xml',
+  theme: 'yonce'
+})
+const cssEditor = CodeMirror(document.querySelector('.editor .code .css-code'), {
+  lineNumbers: true,
+  tabSize: 2,
+  mode: 'css',
+  theme: 'yonce'
+})
+const jsEditor = CodeMirror(document.querySelector('.editor .code .js-code'), {
+  lineNumbers: true,
+  tabSize: 2,
+  mode: 'javascript',
+  theme: 'yonce'
+})
+document.querySelector('#run-btn').addEventListener('click', function(){
+  const htmlCode = htmlEditor.getValue()
+  const cssCode = `<style>${cssEditor.getValue()}</style>`
+  const jsCode = `<script>${jsEditor.getValue()}</script>`
+  const previewWindow = document.querySelector('#preview-window').contentWindow.document 
+  previewWindow.open()
+  previewWindow.write(htmlCode+cssCode+jsCode)
+  previewWindow.close()
+})
+document.querySelector('#download-btn').addEventListener('click', function(){
+  console.log('link clicked !')
+  document.querySelector('#run-btn').click()
+  const previewWindow = document.querySelector('#preview-window').contentWindow.document 
+  const textFileAsBlob = new Blob([previewWindow.documentElement.outerHTML], {type:'text/html'});
+  this.href = window.URL.createObjectURL(textFileAsBlob);
+})
